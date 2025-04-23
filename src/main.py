5f75c176090313a9288c00ddb7433530eb1d5190
vivek singh <viveksingh00747@gmail.com>
2025-04-23 23:37:33 +0530

    added test code for ipynb

--- src/main.py
+++ src/main.py
@@ -12,3 +12,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item": item_id, "q": q}
+
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
